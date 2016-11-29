654b0b8540e4f172cc8c75c32c0089707009b03a
Elviond <Elviond@users.noreply.github.com>
2016-11-29 16:47:28 +0100

    Update Movie.py

--- Movie.py
+++ Movie.py
@@ -1,16 +1,15 @@
 import subprocess
+import os
 
 class MovieHandler(object):
     def __init__(self, path, FPS):
-        if(path[-1] == '/'):
-            self.path = path
-        else:
-            self.path = path + '/'
-        self.FPS = path
+        self.path = path
+        os.chdir(path)
 
-    def resizeImage(self):
-        pass
+    def resizeImage(self, lenght, width):
+        p = subprocess.Popen('convert *.jpg -geometry'.format(self.FPS), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return p.communicate()
 
     def makeVideo(self):
-        p = subprocess.Popen('mencoder {}*.jpg on:fps={}:type=jpeg -ovc lavc -lavcopts vcodec=mjpeg -o timelapse.avi'.format(self.path, self.FPS), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen('avconv -r {FPS} -i photo_%d.jpg video.mp4'.format(self.FPS), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return p.communicate()
