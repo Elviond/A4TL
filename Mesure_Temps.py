08fdb0d491d9aee5a60bfeefb7b6e09f990535e3
Elviond <Elviond@users.noreply.github.com>
2016-12-13 17:22:25 +0100

    Create Mesure_Temps.py

---
+++ Mesure_Temps.py
@@ -0,0 +1,22 @@
+import picamera
+import time
+
+camera = picamera.PiCamera()
+camera.exposure_mode = 'auto'
+
+def writeMesures(temps_pose, temps, i):
+    with open("temps_pose.csv", 'a') as temps_pose_file:
+        temps_pose_file.write(str(temps_pose).replace('.', ',') + '\n')
+    with open("temps.csv", 'a') as temps_file:
+        temps_file.write(str(temps).replace('.', ',') + '\n')
+
+    print(str(temps) + ' ' + str(temps_pose) + ' {} / {}'.format(i+1, etendue))
+
+start_time = time.time()
+etendue = 1440
+for i in range(0, etendue):
+    t0 = time.time()
+    camera.capture("mesure.jpg")
+    temps_pose = time.time() - t0
+    writeMesures(temps_pose, time.time() - start_time, i)
+
