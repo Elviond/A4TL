d210d8251edab8f242ddd3b2c9527506611c054d
Elviond <Elviond@users.noreply.github.com>
2016-12-13 17:19:51 +0100

    Create Server.py

---
+++ Server.py
@@ -0,0 +1,22 @@
+from Networking import Server
+import TimeLapse
+import Movie
+
+
+server = Server('', 12345)
+server.accept()
+
+while True:
+    data = server.recv()
+    print(data)
+
+    if data['Action'] == 'Timelapse':
+        timelapse = TimeLapse.TimeLapse(data["Ordre"][0])
+        timelapse.startTimeLapse(data["Ordre"][1], data["Ordre"][2], data["Ordre"][3], data["Ordre"][4])
+
+        movie = Movie.MovieHandler(data["Ordre"][0])
+        movie.makeVideo()
+
+        server.send({'Ordre':'Etat_Timelapse', 'Action':True})
+
+server.close()
