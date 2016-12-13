fff1d9cc9a2f131e3668ac0a3c9098f85e356b6d
Elviond <Elviond@users.noreply.github.com>
2016-12-13 17:18:08 +0100

    Update TimeLapse.py

--- TimeLapse.py
+++ TimeLapse.py
@@ -2,7 +2,8 @@ import picamera
 import os
 import time
 import threading
-import datetime
+import subprocess
+import Movie
 
 def runAsync(fn):
     def run(*k, **kw):
@@ -10,6 +11,17 @@ def runAsync(fn):
         t.start()
         return t
     return run
+
+class Timer(object):
+    def __init__(self):
+        self.oldTime = -1
+
+    def startTimer(self):
+        self.oldTime = round(time.time(), 3)
+    def stopTimer(self):
+        self.newTime = round(time.time(), 3)
+    def getDifference(self):
+        return self.newTime - self.oldTime
 
 class TimeLapse(object):
     def __init__(self, path):
@@ -23,9 +35,9 @@ class TimeLapse(object):
 
         #Camera Raspberry
         self.camera = picamera.PiCamera()
-        #self.camera.resolution = (1920, 1080)
+        self.camera.resolution = (1920, 1080)
 
-        self.ISO = 200
+        self.ISO = 300
         self.camera.exposure_mode = 'sports'
 
         self.FPS = None
@@ -36,30 +48,47 @@ class TimeLapse(object):
     def capturePhoto(self,number):
         #self.TIMESTAMP = time.strftime("%d-%m-%Y_%H-%M-%S")
         #self.camera.capture('photo_{}.jpg'.format(self.TIMESTAMP))
-        self.camera.capture('photo_{}.jpg'.format(i))
+        self.camera.capture('photo_{}.jpg'.format(number))
 
-    def startTimeLapse(self, FPS, tpsRendu, tpsFonctionnement):
+    def startTimeLapse(self, FPS, tpsRendu, tpsFonctionnement, isAsync=True):
         self.FPS = FPS
         self.tpsRendu = tpsRendu
         self.tpsFonctionnement = tpsFonctionnement
         self.interval = (tpsFonctionnement*3600) / (FPS* tpsRendu)
 
-        for i in range(0, self.FPS * self.tpsRendu - 1):
+        for i in range(0, self.FPS * self.tpsRendu):
             print("{} / {}".format(i+1, self.FPS * self.tpsRendu))
-            self.oldTime = int(round(time.time() * 1000))
-            self.capturePhoto(i)
-            self.delay = self.interval - (int(round(time.time() * 1000)) - self.oldTime)*0.001
 
-            if self.delay <= 0:
-                continue
+            #TODO: Check si interval < 8s
+
+            if isAsync:
+                threading._start_new_thread(target=self.capturePhoto, args=i)
             else:
-                time.sleep(self.delay)
+                self.oldTime = int(round(time.time()) * 1000)
+                self.capturePhoto(i)
+                self.delay = self.interval - (int(round(time.time() * 1000)) - self.oldTime) * 0.001
+
+                if self.delay <= 0:
+                    continue
+                else:
+                    time.sleep(self.delay)
+
 
     def setSharpness(self, sharpness):
         self.camera.sharpness = sharpness
     def setISO(self, ISO):
         self.camera.iso = ISO
+    def makeVideo(self):
+        pass
+    def sendVideo(self):
+        #logging.info("Serveur video demare")
+        p = subprocess.Popen('nc -l -p 12345 < TimeLapse.mp4', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return p.communicate()
 
+path = "/home/pi/photo2"
 
-timelapse = TimeLapse("/home/pi/photo2")
-timelapse.startTimeLapse(24, 30, 0.25)
+timelapse = TimeLapse(path)
+timelapse.startTimeLapse(24, 30, 0.25, False)
+movie = Movie.MovieHandler(path)
+movie.makeVideo()
+timelapse.sendVideo()
